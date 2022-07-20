a679da09d5fba5fda987486ae60a26124159af74
Sanjeev <sanjeevpoudel111@gmail.com>
2022-07-20 22:36:09 +0545

    Update: qr-code show view as svg

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,16 +9,18 @@ class PagesController < ApplicationController
   
   def qr_code_view
     #as PNG
-    send_data RQRCode::QRCode.new(params[:content].to_s).as_png(size: 300), type: 'image/png', disposition: 'inline'    
-    
-    #as SVG
-    # qr_code = RQRCode::QRCode.new(params[:content].to_s)
-    # @svg = qr_code.as_svg(
-    #   offset: 0,
-    #   color: '000',
-    #   shape_rendering: 'crispEdges',
-    #   module_size: 6
-    # )
+    # send_data RQRCode::QRCode.new(params[:content].to_s).as_png(size: 300), type: 'image/png', disposition: 'inline'    
+  
+    # as SVG
+    qr_code = RQRCode::QRCode.new(params[:content].to_s)
+    @svg = qr_code.as_svg(
+      offset: 0,
+      size: 300,
+      color: '000',
+      shape_rendering: 'crispEdges',
+      module_size: 15,
+      standalone: true
+    )
   end
   
 end
