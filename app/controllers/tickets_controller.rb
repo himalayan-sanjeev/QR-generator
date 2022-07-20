158b1d501b9c20ca28d4514565ce55130e00f8d8
Sanjeev <sanjeevpoudel111@gmail.com>
2022-07-20 23:03:24 +0545

    Initialize ticket model and controller

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,10 @@
+class TicketsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def download
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'tickets/index'
+  get 'tickets/show'
+  get 'tickets/download'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
