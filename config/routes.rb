820d1ffa964f396301dabd652da2ed5441a93db3
Sanjeev <sanjeevpoudel111@gmail.com>
2022-07-21 23:10:22 +0545

    Generate unique QR code for each ticket booked

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'tickets/index'
-  get 'tickets/show'
-  get 'tickets/download'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'tickets#index'
+  resources :tickets, only: %i[index create new show download]
 end
