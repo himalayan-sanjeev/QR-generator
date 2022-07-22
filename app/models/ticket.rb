9e6ba22c36809b55e94e05ee3c248cbe2e7a0404
Sanjeev <sanjeevpoudel111@gmail.com>
2022-07-22 07:18:25 +0545

    Updates

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,8 +1,15 @@
 class Ticket < ApplicationRecord
+  validates :info, presence: true  
   after_create :generate_qr_code
 
   def generate_qr_code
-    self.info = SecureRandom.hex
+    # self.info = SecureRandom.hex
+    booked_at = self.created_at.strftime('-%m-%d-%Y-%H-%M-%S')
+    self.info = info.to_s + booked_at.to_s
     save
+  end
+
+  def unique_ticket_id
+    id && ("ticket%.4d" % id)
   end
 end
